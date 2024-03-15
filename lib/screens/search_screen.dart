f09fef6f5607c6ebcd21556315f1e74cf3d8eb6c
ahmedsaadbadawy <ahmedzack168@gmail.com>
2024-03-15 20:58:19 +0200

    implement search screen ui (part 1)

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,16 +1,76 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
+import '../consts/app_images.dart';
 import '../widgets/title_text.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
   @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  late TextEditingController searchTextController;
+
+  @override
+  void initState() {
+    searchTextController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    searchTextController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: TitlesTextWidget(label: "SearchScreen"),
-      ),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            title: const TitlesTextWidget(label: "Search"),
+            leading: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(AssetsManager.imagesBagShoppingCart),
+            ),
+          ),
+          body: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: searchTextController,
+                  decoration: InputDecoration(
+                    filled: true,
+                    prefixIcon: const Icon(Icons.search),
+                    suffixIcon: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          searchTextController.clear();
+                          FocusScope.of(context).unfocus();
+                        });
+                      },
+                      child: const Icon(
+                        Icons.clear,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ),
+                  onChanged: (value) {},
+                  onSubmitted: (value) {
+                    log(searchTextController.text);
+                  },
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
